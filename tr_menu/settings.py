50c24f1c7b574c12559c3b4397cabf2f31ff8ba5
Net <tarasyuk93@inbox.ru>
2024-08-18 17:02:16 +0500

    меню

--- tr_menu/settings.py
+++ tr_menu/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'tr_menu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hh_work',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '12345'
     }
 }
 
